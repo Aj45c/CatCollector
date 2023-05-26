cd6b0626e9dadef60c994cb891e727ff530aa258
Anthony <anthony.jose53@gmail.com>
2023-05-26 19:21:46 -0400

    added a cat model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Danky', 'tortoise shell', 'diluted tortoise shell', 1),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+    
--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html')

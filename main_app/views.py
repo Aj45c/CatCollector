375c08993f31e7ac3508d3fbcb0074c99565108e
Anthony <anthony.jose53@gmail.com>
2023-05-27 14:00:22 -0400

    upadted model and imported in views

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cat
 # Create your views here.
 
+    
 def home(request):
     return HttpResponse('<h1>Hey Hey hey yoyo </h1>')
 
@@ -9,4 +11,5 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
-    return render(request, 'cats/index.html')
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', { 'cats': cats })
